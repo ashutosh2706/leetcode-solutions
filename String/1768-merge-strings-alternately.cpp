f0d62d9a8b0db70ee6369920a85db4e160b51212
ashutosh2706 <imashutosh2706@gmail.com>
2023-04-18 11:58:13 +0530

    add-solution

---
+++ String/1768-merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string res = "";
+        int i = 0, j = 0;
+        while(i < word1.length() && j < word2.length()) {
+            res.push_back(word1[i++]);
+            res.push_back(word2[j++]);
+        }
+
+        while(i<word1.length()) res.push_back(word1[i++]);
+        while(j<word2.length()) res.push_back(word2[j++]);
+        return res;
+    }
+};
