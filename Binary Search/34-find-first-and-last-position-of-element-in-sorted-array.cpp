87039c8561fac8b345688a75af2542c8d9f0cc18
ashutosh2706 <imashutosh2706@gmail.com>
2023-05-28 21:11:34 +0530

    add-solution

--- Binary Search/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ Binary Search/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,13 @@
 class Solution {
-private:
-    int first(vector<int>& nums, int target) {
-    int a=0, b=nums.size()-1,mid;
-    mid=b/2;
-    int res=-1;
-    while(a<=b) {
-        if(nums[mid]>target) b=mid-1;
-        else if(nums[mid]<target) a=mid+1;
-        else if(nums[mid]==target) { res=mid; b=mid-1; }
-        mid=(a+b)/2;
-    }
-    return res; 
-    }
-    
-    int last(vector<int>& nums, int target) {
-    int a=0, b=nums.size()-1,mid;
-    mid=b/2;
-    int res=-1;
-    while(a<=b) {
-        if(nums[mid]>target) b=mid-1;
-        else if(nums[mid]<target) a=mid+1;
-        else if(nums[mid]==target) { res=mid; a=mid+1; }
-        mid=(a+b)/2;
-    }
-    return res;
-    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> res;
-        res.push_back(first(nums,target));
-        res.push_back(last(nums,target));
-        return res;
+        auto it = lower_bound(begin(nums), end(nums), target);
+        auto it2 = upper_bound(begin(nums), end(nums), target);
+        if(it!=end(nums) and it2!=begin(nums) and *it==target) {
+            int first = it-begin(nums);
+            int last = --it2-begin(nums);
+            return vector<int>{first, last};
+        }
+        return vector<int>{-1,-1};
     }
 };
