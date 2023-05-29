0a5f3af5f261d54b621a99a1a6cbbf32965b4b92
ashutosh2706 <imashutosh2706@gmail.com>
2023-05-29 19:14:24 +0530

    modify-solution

--- Arrays/189-rotate-array.cpp
+++ Arrays/189-rotate-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> tmp(begin(nums), end(nums));
-        for(int i=nums.size()-1; i>=0; i--) {
-            int index = (i+k) % nums.size();
-            nums[index] = tmp[i];
-        }
+        reverse(begin(nums),end(nums));
+        int l = 0, r = k-1;
+        while(l < r) swap(nums[l++], nums[r--]);
+        l = k, r=nums.size()-1;
+        while(l<r) swap(nums[l++], nums[r--]);
     }
 };
