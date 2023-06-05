25875bae632856242f8acdf3bd818dc4e46aa484
ashutosh2706 <imashutosh2706@gmail.com>
2023-06-05 12:50:39 +0530

    add-solution

---
+++ Greedy/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& time) {
+
+    int n = colors.length(), ans = 0;
+    for(int i=0; i<n;) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        char col = colors[i];
+        while(i < n and colors[i] == col) {
+            pq.push(time[i]);
+            i++;
+        }
+        while(pq.size() > 1) {
+            ans += pq.top();
+            pq.pop();
+        }
+    }
+    return ans;
+    }
+};
