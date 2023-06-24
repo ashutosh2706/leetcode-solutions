505481327ec714695f02f5c01428181deeca5b46
ashutosh2706 <imashutosh2706@gmail.com>
2023-06-24 10:16:41 +0530

    add-solution

---
+++ Arrays/53-maximum-subarray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN, sum = 0;
+        for(int i=0; i<nums.size(); i++) {
+            sum = max(nums[i], sum + nums[i]);
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
