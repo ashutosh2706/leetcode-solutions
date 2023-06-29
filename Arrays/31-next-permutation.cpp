5bdc33e9580edd0d20ac919573f7248ce34f2d7d
ashutosh2706 <imashutosh2706@gmail.com>
2023-06-29 15:41:35 +0530

    add-solution

---
+++ Arrays/31-next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int N = nums.size(), ind = -1;
+        for(int i=N-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(int i=N-1; i>=ind; i--) {
+            if(nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin()+ind+1, nums.end());
+        return;
+    }
+};
