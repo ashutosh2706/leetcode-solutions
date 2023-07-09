4f46b2e191f4175349a9d90f9852919520924d72
ashutosh2706 <imashutosh2706@gmail.com>
2023-07-09 19:49:38 +0530

    modify-solution

--- Dynamic Programming/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ Dynamic Programming/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,33 +1,25 @@
 class Solution {
-private:
-    int dp[1005][1005];
-
-int solve(int ind, int prev, vector<int>& nums, int target) {
-    if(ind >= nums.size()) return INT_MIN;
-    if(ind == nums.size() - 1) 
-    {   
-        if((nums[ind] - nums[prev]) >= -target and (nums[ind] - nums[prev]) <= target)
-        return 1;
-        else return INT_MIN;
-    }
-
-    if(dp[ind][prev] != -1) return dp[ind][prev];
-    int ans = INT_MIN;
-
-    if((nums[ind] - nums[prev]) >= -target and (nums[ind] - nums[prev]) <= target) {
-        // out(ind) out(prev)
-        ans = max(ans, 1 + solve(ind+1, ind, nums, target));
-    }
-
-    ans = max(ans, 0 + solve(ind+1, prev, nums, target));
-    return dp[ind][prev] = ans;
-}
 public:
-    
+    int dp[1005];
+    int solve(int ind, vector<int> &nums, int t) {
+        
+        if(ind == nums.size()-1) return 0;
+        
+        if(dp[ind] != -1) return dp[ind];
+        
+        int ans = INT_MIN;
+        for(int i=ind+1; i < nums.size(); i++) {
+            if((nums[i] - nums[ind]) >= -t and (nums[i] - nums[ind]) <= t) {
+                ans = max(ans, 1+solve(i, nums, t));
+            }
+        }
+        
+        return dp[ind] = ans;
+    }
     int maximumJumps(vector<int>& nums, int target) {
         memset(dp,-1,sizeof(dp));
-        int x = solve(1,0,nums,target);
-        if(x <= 0) return -1;
-        return x;
+        int ans = solve(0,nums,target);
+        if(ans <= 0) return -1;
+        return ans;
     }
 };
