a4b087f5da49e57d54fb6731ed69b949e75b02ae
ashutosh2706 <imashutosh2706@gmail.com>
2023-07-10 22:34:04 +0530

    add-solution

---
+++ Two Pointers/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+
+        sort(begin(nums),end(nums));
+        int  l = 0, r = nums.size()-1, ans = 0;
+        while(l < r) {
+            if(nums[l] + nums[r] == k) {
+                ans++;
+                l++;
+                r--;
+            } else if(nums[l] + nums[r] > k) r--;
+            else l++;
+        }
+        return ans;
+    }
+};
