af2c73f47c0e72814cedc56318381f281d693089
ashutosh2706 <imashutosh2706@gmail.com>
2023-07-13 00:29:09 +0530

    modify-solution

--- Arrays/523-continuous-subarray-sum.cpp
+++ Arrays/523-continuous-subarray-sum.cpp
@@ -1,23 +1,22 @@
-// Prefix Sum + Hashing
-
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-
-        vector<long long> v(nums.size());
-        map<int, int> map;
-        v[0] = nums[0];
-        map[nums[0] % k] = 0;
-        for(int i=1; i<nums.size(); i++) {
-            v[i] = nums[i] + v[i-1];
-            int rem = v[i] % k;
-            if(rem == 0) return 1;
-            if(map.count(rem)) {
-                if(i - map[rem] >= 2) return 1; 
-            }else map[rem] = i;
+        
+        vector<long long> v(begin(nums), end(nums));
+        int N = nums.size();
+        unordered_map<long long,int> map;
+        for(int i=1; i<N; i++) {
+            v[i] += v[i-1];
+        }
+        map[0] = -1;
+        for(int i=0; i<N; i++) {
+            long long mod = v[i] % k;
+            if(map.count(mod)) {
+                if(i - map[mod] >= 2) return 1;
+            } else {
+                map[mod] = i;
+            }
         }
         return 0;
-
     }
 };
