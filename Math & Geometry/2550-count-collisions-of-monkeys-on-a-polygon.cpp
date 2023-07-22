4d6388c36244cbdf506c78bcbd78110b6c274e0a
ashutosh2706 <imashutosh2706@gmail.com>
2023-07-22 14:55:11 +0530

    add-solution

---
+++ Math & Geometry/2550-count-collisions-of-monkeys-on-a-polygon.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+int power(long long x, unsigned int y, int p)
+{
+    int res = 1;     // Initialize result
+ 
+    x = x % p; // Update x if it is more than or
+                // equal to p
+  
+    if (x == 0) return 0; // In case x is divisible by p;
+ 
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+ 
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
+}
+    int monkeyMove(int n) {
+        unsigned int MOD = 1e9+7;
+        int ways = power(2,n,MOD) - 2;
+        if(ways < 0) return ways + MOD;
+        return ways;
+    }
+};
