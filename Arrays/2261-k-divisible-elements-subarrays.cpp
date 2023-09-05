15b0ce0beae7b3d7a91aa7b1e22e25cb215cf586
ashutosh2706 <imashutosh2706@gmail.com>
2023-09-05 22:40:31 +0530

    add-solution

---
+++ Arrays/2261-k-divisible-elements-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) {
+    set<std::vector<int>> set;
+    for(int i=0; i<nums.size(); i++) {
+        vector<int> v;
+        int cnt = 0;
+        for(int j=i; j<nums.size(); j++) {
+            v.emplace_back(nums[j]);
+            if(nums[j]%p == 0) cnt++;
+            if(cnt <= k) set.insert(v);
+        }
+    }
+    return (int)set.size();
+    }
+};
