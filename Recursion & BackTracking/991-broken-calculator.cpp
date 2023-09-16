fa3d747b442b445d453d2513776031f9ff104e4f
ashutosh2706 <imashutosh2706@gmail.com>
2023-09-16 15:34:03 +0530

    add-solution

---
+++ Recursion & BackTracking/991-broken-calculator.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int brokenCalc(int s, int t) {
+        if(s >= t) return s-t;
+        if(t&1) return 1 + brokenCalc(s, t+1);
+        return 1 + brokenCalc(s, t/2);
+    }
+};
