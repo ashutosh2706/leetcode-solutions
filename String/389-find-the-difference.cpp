b757e4904aa0372b30e9892588b7c65578744f7e
ashutosh2706 <imashutosh2706@gmail.com>
2023-09-25 23:38:58 +0530

    add-solution

---
+++ String/389-find-the-difference.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(begin(s), end(s));
+        sort(begin(t), end(t));
+        for(int i=0; i<s.length(); ++i) {
+            if(s[i] != t[i]) return t[i];
+        }
+        return t.back();
+    }
+};
