0abd0d02f5927fe3bf4f8c9bba7a9447ba6c25db
ashutosh2706 <imashutosh2706@gmail.com>
2023-11-24 00:16:11 +0530

    add-solution

---
+++ Arrays/1424-diagonal-traverse-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int m=nums.size(),n=0;
+        for(int i=0;i<m;i++){
+            n = max(n, (int)nums[i].size());
+        }
+        vector<vector<int>> temp(m+n);
+        vector<int>ans;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<nums[i].size();j++){
+                temp[i+j].push_back(nums[i][j]);
+            }
+        } 
+        for(int i=0;i<m+n;i++){
+            reverse(temp[i].begin(),temp[i].end());
+        }
+         for(int i=0;i<m+n;i++){
+            for(int j=0;j<temp[i].size();j++){
+                ans.push_back(temp[i][j]);
+            }
+        }
+        return ans;
+    }
+};
