23f4562f90ddf9c7beb5362d2bb3671c639dfd4c
ashutosh2706 <imashutosh2706@gmail.com>
2023-12-21 21:59:13 +0530

    add-solution

---
+++ Math & Geometry/1637-widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+        sort(begin(points), end(points), [](auto &a, auto &b){
+            return a[0] < b[0];
+        });
+        int ans = 0;
+        for(int i=1; i<points.size(); ++i) {
+            ans = max(ans, points[i][0] - points[i-1][0]);
+        }
+        return ans;
+    }
+};
